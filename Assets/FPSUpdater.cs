c554c3cb48a028f4da201eda60f22b6e6c71d443
Kevin Cox <kevintcox@gmail.com>
2017-05-10 18:21:58 -0400

    Move stuff, add video example

---
+++ Assets/FPSUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSUpdater : MonoBehaviour {
+
+	Text fpsText;
+	// Use this for initialization
+	void Start () {
+		fpsText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.frameCount % 10 == 0) {
+			int fps = (int) (1 / Time.smoothDeltaTime);
+			fpsText.text = fps.ToString();//string.Format("{0} FPS", fps);
+		}
+	}
+}
---
+++ Assets/VideoLoader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoLoader : MonoBehaviour {
+
+	VideoPlayer[] videoPlayers = new VideoPlayer[2];
+	int videoIndex = -1;
+	// int activeVideoPlayerIndex = 0;
+	VideoPlayer activeVideoPlayer;
+	Renderer videoRenderer;
+
+	void Start () {
+		Application.targetFrameRate = 300;
+		videoRenderer = this.gameObject.GetComponent<Renderer>();
+		for (int i = 0; i < videoPlayers.Length; i++) {
+			videoPlayers[i] = AddVideoPlayer();
+		}
+		LoadAndPlayNext();
+	}
+	
+	void Update () {
+		if (Time.frameCount % 200 == 0) {
+			print("load and play next video...");
+			LoadAndPlayNext();
+		}
+	}
+
+	VideoPlayer AddVideoPlayer() {
+		VideoPlayer player = this.gameObject.AddComponent<VideoPlayer>();
+		player.isLooping = true;
+		player.playOnAwake = false;
+		return player;
+	}
+
+	void LoadAndPlayNext() {
+		LoadAndPlayVideo(GetNextVideoPlayer(), GetNextVideoUrl());
+	}
+
+	void LoadAndPlayVideo(VideoPlayer player, string url) {
+		player.url = url;
+		player.prepareCompleted += OnVideoPlayerReady;
+		// player.errorReceived += OnVideoPlayerError;
+		player.Prepare();
+	}
+
+	void OnVideoPlayerReady(VideoPlayer player) {
+		print("OnVideoPlayerReady");
+		player.Play();
+		activeVideoPlayer = player;
+		videoRenderer.material.mainTexture = player.texture;
+		player.prepareCompleted -= OnVideoPlayerReady;
+	}
+
+	// void OnVideoPlayerError(VideoPlayer source, string msg) {
+	// 	print("video player error: " + msg);
+	// }
+
+	VideoPlayer GetNextVideoPlayer() {
+		int i;
+		for (i = 0; i < videoPlayers.Length; i++) {
+			if (videoPlayers[i] == activeVideoPlayer) {
+				break;
+			}
+		}
+		int index = (i + 1) % videoPlayers.Length;
+		return videoPlayers[index];
+	}
+
+	string GetNextVideoUrl() {
+		int index = (videoIndex + 1) % 3;
+		videoIndex = index;
+		return GetVideoUrl(index);
+	}
+
+	string GetVideoUrl(int index) {
+		// return "file:///Users/kevin/Movies/hypno0" + index + ".m4v";
+		return "http://localhost:8080/hypno0" + index + ".m4v";
+	}
+}
